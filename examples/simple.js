3747309c98a9ffea5202415cb13067bee03a3956
Holger Will <h.will@klimapartner.de>
2018-11-30 06:44:49 +0100

    correctly require the parser from parent directory

--- examples/simple.js
+++ examples/simple.js
@@ -1,5 +1,5 @@
 const SerialPort = require('serialport')
-const EnoceanParser = require('node-serialport-enocean-parser');
+const EnoceanParser = require('../ESP3Parser');
 
 const port = new SerialPort('/dev/ttyUSB0',{ baudRate: 57600 })
 const parser = port.pipe(new EnoceanParser())
