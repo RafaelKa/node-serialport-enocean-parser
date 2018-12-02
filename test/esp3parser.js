f4e7f6a0c4d00567d68a0523eaae1e4a34545932
Holger Will <h.will@klimapartner.de>
2018-12-02 08:33:27 +0100

    initial linter fixes

--- test/esp3parser.js
+++ test/esp3parser.js
@@ -1,31 +1,32 @@
-'use strict';
+"use strict"
 
-const assert = require('chai').assert;
-const sinon = require('sinon');
+const assert = require("chai").assert
+const sinon = require("sinon")
 
-const Esp3Parser = require('../ESP3Parser');
-var esp3parser = new Esp3Parser();
+const Esp3Parser = require("../ESP3Parser")
 
+/* eslint-disable no-undef  */
+/* ideally don't disbale here but instead we could use eslint-plugin-mocha or define global var in .eslintrc*/
 var telegrams = {
-    _4BS_A5: '55000a0701eba5c87f710fffdba5e40001ffffffff47000d',
-    _1BS_D5: '55000707017ad509ffdba5ed0001ffffffff470096',
-    _VLD_D2: '55000c070196d24000b00a010001a03d790001ffffffff5b0033',
-    _RPS_F6: '55000707017af600ffd9b7812001ffffffff460050',
-    _4BS_Teach_In_A5:'55000a0701eba540300287ffd9b7e50001ffffffff440016'
-};
+  _4BS_A5: "55000a0701eba5c87f710fffdba5e40001ffffffff47000d",
+  _1BS_D5: "55000707017ad509ffdba5ed0001ffffffff470096",
+  _VLD_D2: "55000c070196d24000b00a010001a03d790001ffffffff5b0033",
+  _RPS_F6: "55000707017af600ffd9b7812001ffffffff460050",
+  _4BS_Teach_In_A5:"55000a0701eba540300287ffd9b7e50001ffffffff440016"
+}
 
 
-describe('serialport enocean parser', function(){
-    describe('ESP3Packet.getRawBuffer()', function() {
-        it('returns identical buffer', function() {
-            for (var key in telegrams) {
-              const telegramm = Buffer.from(telegrams[key], 'hex');
-              const spy = sinon.spy();
-              const parser = new Esp3Parser();
-              parser.on('data', spy);
-              parser.write(telegramm);
-              assert.equal(spy.getCall(0).args[0].getRawBuffer().toString('hex'), telegramm.toString('hex'));
-            }
-        });
-    });
-});
+describe("serialport enocean parser", function(){
+  describe("ESP3Packet.getRawBuffer()", function() {
+    it("returns identical buffer", function() {
+      for (var key in telegrams) {
+        const telegramm = Buffer.from(telegrams[key], "hex")
+        const spy = sinon.spy()
+        const parser = new Esp3Parser()
+        parser.on("data", spy)
+        parser.write(telegramm)
+        assert.equal(spy.getCall(0).args[0].getRawBuffer().toString("hex"), telegramm.toString("hex"))
+      }
+    })
+  })
+})
